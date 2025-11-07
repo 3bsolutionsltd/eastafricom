8253b22213a1665e8c93dafe1d53f744011b4a44
Stephen Omwony <3bsolutionsltd@gmail.com>
2025-11-07 19:56:30 +0300

    Add root-level debug and permission fix files

---
+++ debug.php
@@ -0,0 +1,28 @@
+﻿<?php
+// Simple debug to check deployment status
+echo "<h1>East Africom Debug</h1>";
+echo "<h2>Files in Root Directory:</h2>";
+echo "<pre>";
+$files = scandir('.');
+foreach($files as $file) {
+    if($file != '.' && $file != '..') {
+        echo $file;
+        if(is_dir($file)) echo " [DIR]";
+        echo "\n";
+    }
+}
+echo "</pre>";
+
+echo "<h2>Index.html Status:</h2>";
+if(file_exists('index.html')) {
+    echo " Found index.html<br>";
+    echo "Size: " . filesize('index.html') . " bytes<br>";
+    echo "Permissions: " . substr(sprintf('%o', fileperms('index.html')), -4) . "<br>";
+} else {
+    echo " index.html missing<br>";
+}
+
+echo "<h2>Current Directory:</h2>";
+echo getcwd() . "<br>";
+echo "Permissions: " . substr(sprintf('%o', fileperms('.')), -4) . "<br>";
+?>
---
+++ fix-permissions.php
@@ -0,0 +1,30 @@
+﻿<?php
+echo "<h1>Fixing File Permissions</h1>";
+
+// Fix directory permissions
+$dirs = ['.', 'admin', 'backend', 'css', 'images', 'js', 'pages'];
+foreach($dirs as $dir) {
+    if(is_dir($dir)) {
+        if(chmod($dir, 0755)) {
+            echo " Fixed permissions for directory: $dir<br>";
+        } else {
+            echo " Failed to fix directory: $dir<br>";
+        }
+    }
+}
+
+// Fix file permissions
+$files = ['index.html', 'debug.php'];
+foreach($files as $file) {
+    if(file_exists($file)) {
+        if(chmod($file, 0644)) {
+            echo " Fixed permissions for file: $file<br>";
+        } else {
+            echo " Failed to fix file: $file<br>";
+        }
+    }
+}
+
+echo "<br><a href='index.html'>Try visiting index.html</a><br>";
+echo "<a href='debug.php'>Check debug info</a><br>";
+?>
